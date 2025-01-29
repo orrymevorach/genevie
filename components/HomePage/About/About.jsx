e5744ec3d5f483ad4e7f9e44c5bdb7ef44fa95fc
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2025-01-28 21:00:36 -0500

    home page fonts and spacing

--- components/HomePage/About/About.jsx
+++ components/HomePage/About/About.jsx
@@ -12,7 +12,7 @@ export default function About() {
           <Image src={image} alt="" />
         </div>
         <div className={styles.textContainer}>
-          <p className={styles.title}>About</p>
+          <p className={styles.title}>About Genevie</p>
           <p className={styles.description}>
             Starting a family raises a million questions, many of which can’t be
             fully addressed during limited time with our providers. From the big
@@ -20,21 +20,28 @@ export default function About() {
             have genetic testing, and which option is best?" to the smaller,
             daily concerns such as, "Is this product safe to use?" or "Can I
             take this supplement or medication?"  Information online (or from
-            friends) is often generic, contradictory, or outdated. Despite the
-            overwhelming need for reliable guidance, most families are never
-            referred to a genetic counselor. For the few who are, it’s often a
-            single-visit session later in pregnancy, leaving little time for
-            education, testing or proactive planning. Patients need more than a
-            brief consultation—they need early access to information and ongoing
-            support to make informed testing and family-planning decisions. This
-            critical gap in preconception and prenatal care sparked the creation
-            of GENEVIE—a service designed to address real patient needs. GENEVIE
-            delivers  two pivotal foundations of care: evidence-based guidance
-            before pregnancy and ongoing personalized support throughout,
-            empowering patients to feel confident and informed every step of
-            their reproductive journey. 
+            friends) is often generic, contradictory, or outdated.
           </p>
-          <Button isTertiary>Meet the founder</Button>
+          <p className={styles.description}>
+            Despite the overwhelming need for reliable guidance, most families
+            are never referred to a genetic counselor. For the few who are, it’s
+            often a single-visit session later in pregnancy, leaving little time
+            for education, testing or proactive planning. Patients need more
+            than a brief consultation—they need early access to information and
+            ongoing support to make informed testing and family-planning
+            decisions.
+          </p>
+          <p className={styles.description}>
+            This critical gap in preconception and prenatal care sparked the
+            creation of GENEVIE—a service designed to address real patient
+            needs. GENEVIE delivers two pivotal foundations of care:
+            evidence-based guidance before pregnancy and ongoing personalized
+            support throughout, empowering patients to feel confident and
+            informed every step of their reproductive journey. 
+          </p>
+          <Button isTertiary classNames={styles.button}>
+            Meet the founder
+          </Button>
         </div>
       </Wrapper>
     </div>
