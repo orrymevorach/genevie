a3585ec605a8ee6bc03051ed22ddfb3e7ffc6cb5
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2025-02-01 16:59:11 -0500

    library page

--- components/LibraryPage/LibraryItems/Column/Column.jsx
+++ components/LibraryPage/LibraryItems/Column/Column.jsx
@@ -1,9 +1,10 @@
 import Link from 'next/link';
 import styles from './Column.module.scss';
+import Button from '@/components/shared/Button/Button';
 
 export default function Column({ title, items, buttonText }) {
   return (
-    <div>
+    <div className={styles.column}>
       <p className={styles.columnHeading}>{title}</p>
       <ul>
         {items.map(item => {
@@ -11,9 +12,9 @@ export default function Column({ title, items, buttonText }) {
             <li key={item.title} className={styles.item}>
               <p className={styles.title}>{item.title}</p>
               <p className={styles.author}>{item.author}</p>
-              <Link href={item.link} className={styles.link}>
+              <Button href={item.link} className={styles.link} isLinkStyle>
                 {buttonText}
-              </Link>
+              </Button>
             </li>
           );
         })}
--- components/LibraryPage/LibraryPage.jsx
+++ components/LibraryPage/LibraryPage.jsx
@@ -4,14 +4,15 @@ import Layout from '../shared/Layout/Layout';
 import styles from './LibraryPage.module.scss';
 import LibraryItems from './LibraryItems/LibraryItems';
 import Wrapper from '../shared/Wrapper/Wrapper';
+import HeroContent from '../shared/Hero/HeroContent/HeroContent';
 
 export default function LibraryPage({ entries }) {
   const [banner, libraryItems] = entries;
   const image = getMedia(banner.image);
   return (
     <Layout>
-      <Hero isCenter imageUrl={image.src}>
-        <h2 className={styles.title}>Library</h2>
+      <Hero isCenter imageUrl={image.src} isSmall>
+        <HeroContent text={banner.text} isCenter />
       </Hero>
       <Wrapper>
         <LibraryItems items={libraryItems} />
