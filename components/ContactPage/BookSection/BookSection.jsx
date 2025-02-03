4553388f18c3b5a5f5dee2f80335f8b61500829a
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2025-02-03 17:13:01 -0500

    contact page

--- components/ContactPage/BookSection/BookSection.jsx
+++ components/ContactPage/BookSection/BookSection.jsx
@@ -1,21 +1,24 @@
 import Button from '@/components/shared/Button/Button';
 import styles from './BookSection.module.scss';
 import { useIframeContext } from '@/components/shared/Layout/Layout';
+import Wrapper from '@/components/shared/Wrapper/Wrapper';
 
 export default function BookSection() {
   const { setShowIframe } = useIframeContext();
   return (
     <div className={styles.container}>
-      <h2 className={styles.title}>Book A Consultation</h2>
-      <p className={styles.description}>
-        Ready to get started? Click below to schedule your complimentary
-        10-minute session to explore your goals and questions. Availability is
-        based on limited number of accepts clients per year.
-      </p>
+      <Wrapper classNames={styles.wrapper}>
+        <h2 className={styles.title}>Book A Consultation</h2>
+        <p className={styles.description}>
+          Ready to get started? Click below to schedule your complimentary
+          10-minute session to explore your goals and questions. Availability is
+          based on limited number of accepts clients per year.
+        </p>
 
-      <Button isTertiary handleClick={() => setShowIframe(true)}>
-        Schedule Now
-      </Button>
+        <Button isTertiary handleClick={() => setShowIframe(true)}>
+          Schedule Now
+        </Button>
+      </Wrapper>
     </div>
   );
 }
--- components/ContactPage/ContactPage.jsx
+++ components/ContactPage/ContactPage.jsx
@@ -1,5 +1,6 @@
 // import styles from './ContactPage.module.scss';
 import Layout from '../shared/Layout/Layout';
+import Wrapper from '../shared/Wrapper/Wrapper';
 import BookSection from './BookSection/BookSection';
 import ContactForm from './ContactForm/ContactForm';
 
@@ -7,7 +8,9 @@ export default function ContactPage() {
   return (
     <>
       <BookSection />
-      <ContactForm />
+      <Wrapper>
+        <ContactForm />
+      </Wrapper>
     </>
   );
 }
