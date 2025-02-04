fbbadfe464a5be91e3b906fcd210081e176b72b4
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2025-02-03 22:39:17 -0500

    mobile polish

--- components/LibraryPage/LibraryPage.jsx
+++ components/LibraryPage/LibraryPage.jsx
@@ -4,13 +4,20 @@ import Layout from '../shared/Layout/Layout';
 import styles from './LibraryPage.module.scss';
 import LibraryItems from './LibraryItems/LibraryItems';
 import Wrapper from '../shared/Wrapper/Wrapper';
+import useWindowSize from '@/hooks/useWindowSize';
 
 export default function LibraryPage({ entries }) {
   const [banner, libraryItems] = entries;
   const image = getMedia(banner.image);
+  const { isMobile } = useWindowSize();
   return (
     <Layout>
-      <Hero isCenter imageUrl={image.src} isSmall text={banner.text} />
+      <Hero
+        isCenter
+        imageUrl={image.src}
+        isSmall={!isMobile}
+        text={banner.text}
+      />
       <Wrapper>
         <LibraryItems items={libraryItems} />
       </Wrapper>
--- components/shared/Nav/AppointmentsIframe/AppointmentsIframe.jsx
+++ components/shared/Nav/AppointmentsIframe/AppointmentsIframe.jsx
@@ -1,7 +1,10 @@
+import useWindowSize from '@/hooks/useWindowSize';
 import CloseButton from '../../Takeover/CloseButton/CloseButton';
 import styles from './AppointmentsIframe.module.scss';
 
 export default function AppointmentsIframe({ setShowIframe }) {
+  const { isMobile } = useWindowSize();
+  const width = isMobile ? '90%' : '600px';
   return (
     <div className={styles.iframeContainer}>
       <div className={styles.overlay}></div>
@@ -10,9 +13,11 @@ export default function AppointmentsIframe({ setShowIframe }) {
         <iframe
           src="https://secure.gethealthie.com/appointments/embed_appt?dietitian_id=7113289&provider_ids=%5B7113289%5D&appt_type_ids=%5B443516,443517,443518%5D"
           style={{
-            width: '600px',
+            width,
             minHeight: '600px',
             border: '0px',
+            margin: '0 auto',
+            display: 'block',
           }}
           className={styles.iframe}
         ></iframe>
