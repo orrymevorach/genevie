e6944f31b4189864484eca08fe696b40c3128119
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2025-02-11 00:18:34 -0500

    about page update

--- components/AboutPage/AboutPage.jsx
+++ components/AboutPage/AboutPage.jsx
@@ -9,23 +9,29 @@ import Societies from './Societies/Societies';
 import RichText from '../shared/RichText/RichText';
 import Reveal from '../shared/Reveal/Reveal';
 import useWindowSize from '@/hooks/useWindowSize';
+import Image from 'next/image';
+import { getMedia } from '@/lib/contentful-utils';
 
 export default function AboutPage({ entries }) {
-  const [
-    banner,
-    aboutRichText,
-    education,
-    instructor,
-    societies,
-    publications,
-  ] = entries;
+  const [image, aboutRichText, education, instructor, societies, publications] =
+    entries;
   const { isMobile } = useWindowSize();
   const Element = !isMobile ? Reveal : 'div';
+  const { src, width, height, alt } = getMedia(image.image);
   return (
     <Layout>
-      <Hero bannerFields={banner} shouldAnimate />
       <Wrapper isSmall>
         <Element className={styles.textContainer}>
+          <h1 className={styles.meet}>Meet the Founder</h1>
+          <h2 className={styles.lila}>Lila Dayani</h2>
+          <p className={styles.qualifications}>MS, MS, LCGC</p>
+          <Image
+            src={src}
+            alt={alt}
+            width={width}
+            height={height}
+            className={styles.image}
+          />
           <RichText json={aboutRichText.richText} />
         </Element>
         <div className={styles.container}>
