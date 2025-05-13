5cadcef41c74d05d4db7138e6a58637e77da6247
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2025-05-12 18:22:01 -0700

    seo improvements

--- components/shared/Head/Head.jsx
+++ components/shared/Head/Head.jsx
@@ -7,7 +7,7 @@ export default function Meta() {
       <meta charSet="utf-8" />
       <meta
         name="description"
-        content="GENEVIE is a first-of-its kind concierge health service, led by reproductive genetic experts, offering comprehensive evaluations, personalized genetic testing, and tailored scientific support for every stage of your journey."
+        content="GENEVIE is a first-of-its kind concierge health service founded by Lila Dayani. Led by reproductive genetic experts, offering comprehensive evaluations, personalized genetic testing, and tailored scientific support for every stage of your journey."
       />
       <meta name="keywords" content="" />
       <title>{tabTitle}</title>
