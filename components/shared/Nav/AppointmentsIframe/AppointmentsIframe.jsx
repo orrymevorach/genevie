9aa6a7164242576953da6f278fb3208bd29c7b89
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2025-11-17 13:16:54 -0500

    appointments iframe text background

--- components/shared/Nav/AppointmentsIframe/AppointmentsIframe.jsx
+++ components/shared/Nav/AppointmentsIframe/AppointmentsIframe.jsx
@@ -7,16 +7,6 @@ export default function AppointmentsIframe({ setShowIframe }) {
     <div className={styles.container}>
       <div className={styles.overlay}></div>
       <div className={styles.iframeContainer}>
-        <div className={styles.textContainer}>
-          <p>
-            Ready to book an appointment? Fill out the form below, or reach out
-            to us anytime at{' '}
-            <CopyToClipboard text="info@geneviehealth.com">
-              <span className={styles.bold}>info@geneviehealth.com</span>
-            </CopyToClipboard>
-            .
-          </p>
-        </div>
         <div className={styles.closeButton}>
           <CloseButton handleClick={() => setShowIframe(false)} />
         </div>
@@ -32,6 +22,16 @@ export default function AppointmentsIframe({ setShowIframe }) {
           }}
           className={styles.iframe}
         ></iframe>
+        <div className={styles.textContainer}>
+          <p>
+            Ready to book an appointment? Fill out the form below, or reach out
+            to us anytime at{' '}
+            <CopyToClipboard text="info@geneviehealth.com">
+              <span className={styles.bold}>info@geneviehealth.com</span>
+            </CopyToClipboard>
+            .
+          </p>
+        </div>
       </div>
     </div>
   );
