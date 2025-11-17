5d554d3b95fc86daae049235e34774618d2e834c
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2025-11-17 12:36:07 -0500

    appointments iframe text

--- components/shared/Nav/AppointmentsIframe/AppointmentsIframe.jsx
+++ components/shared/Nav/AppointmentsIframe/AppointmentsIframe.jsx
@@ -1,20 +1,30 @@
-import useWindowSize from '@/hooks/useWindowSize';
+import CopyToClipboard from '../../CopyToClipboard/CopyToClipboard';
 import CloseButton from '../../Takeover/CloseButton/CloseButton';
 import styles from './AppointmentsIframe.module.scss';
 
 export default function AppointmentsIframe({ setShowIframe }) {
-  const { isDesktop } = useWindowSize();
-  const width = !isDesktop ? '90%' : '1100px';
   return (
-    <div className={styles.iframeContainer}>
+    <div className={styles.container}>
       <div className={styles.overlay}></div>
-      <div className={styles.closeButtonContainer}>
-        <CloseButton handleClick={() => setShowIframe(false)} />
+      <div className={styles.iframeContainer}>
+        <div className={styles.textContainer}>
+          <p>
+            Ready to book an appointment? Fill out the form below, or reach out
+            to us anytime at{' '}
+            <CopyToClipboard text="info@geneviehealth.com">
+              <span className={styles.bold}>info@geneviehealth.com</span>
+            </CopyToClipboard>
+            .
+          </p>
+        </div>
+        <div className={styles.closeButton}>
+          <CloseButton handleClick={() => setShowIframe(false)} />
+        </div>
 
         <iframe
           src="https://secure.gethealthie.com/appointments/embed_appt?dietitian_id=7113289&provider_ids=%5B7113289%5D&appt_type_ids=%5B443516,445794,443517,445796,443518%5D"
           style={{
-            width,
+            width: '100%',
             minHeight: '600px',
             border: '0px',
             margin: '0 auto',
